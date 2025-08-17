aac892ee6ae9d9a32d80d05b946d074ba3d2c27c
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-17 14:30:59 +0300

    Initial Commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Михайло Тихонов on 17.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
